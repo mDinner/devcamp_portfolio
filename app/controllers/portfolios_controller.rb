8688ed7d513330c8a5f078d8949f12c2d2e56870
Michael Dinner <mdinner9@gmail.com>
2017-04-26 10:48:33 -0400

    iplemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,17 +7,33 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
     end
 
-  def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+	def create
+	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-        format.json { render :show, status: :created, location: @portfolio_item }
-      else
-        format.html { render :new }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	respond_to do |format|
+	  if @portfolio_item.save
+	    format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+	    format.json { render :show, status: :created, location: @portfolio_item }
+	  else
+	    format.html { render :new }
+	    format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+	  end
+	end
+	end
+
+  	def edit
+  		@portfolio_item = Portfolio.find(params[:id])
+  	end
+
+	def update
+  		@portfolio_item = Portfolio.find(params[:id])
+
+		respond_to do |format|
+		  if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+		    format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+		  else
+		    format.html { render :edit }
+		  end
+		end
+	end
 end
